f75afaf5d65c2b373dc18685827c84a8f8771f99
Orr Paradise <orrp@eecs.berkeley.edu>
2024-10-23 16:49:28 -0700

    lint: explicitly pass toml-config so that flake8 fails if flake8-pyproject not found

--- lint.sh
+++ lint.sh
@@ -3,6 +3,6 @@ isort spm
 echo ">>> black"
 black spm
 echo ">>> flake8"
-flake8 spm
+flake8 --toml-config pyproject.toml spm # explicitly pass toml-config so that this fails if flake8-pyproject not found
 echo ">>> mypy"
 mypy --cache-dir ../.mypy_cache spm
